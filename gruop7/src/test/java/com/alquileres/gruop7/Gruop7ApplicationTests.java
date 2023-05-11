837d3d22dbac9a9ca936bb1a84216e44f2e84f7d
Tania Huallpa <73077233+Madelin-Aov@users.noreply.github.com>
2023-05-11 19:51:38 -0300

    Add files via upload
    First commit, creacion de archivo mediante Spring Initializr.
    
    Signed-off-by: Tania Huallpa <73077233+Madelin-Aov@users.noreply.github.com>

---
+++ gruop7/src/test/java/com/alquileres/gruop7/Gruop7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquileres.gruop7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruop7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
