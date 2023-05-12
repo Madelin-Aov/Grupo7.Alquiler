9e9749b21d753419f0ec0dcc8126a188f31c850c
Tania Huallpa <73077233+Madelin-Aov@users.noreply.github.com>
2023-05-11 21:26:53 -0300

    Add files via upload
    Signed-off-by: Tania Huallpa <73077233+Madelin-Aov@users.noreply.github.com>

---
+++ gruop7/src/main/java/com/alquileres/gruop7/Gruop7Application.java
@@ -0,0 +1,13 @@
+package com.alquileres.gruop7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruop7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gruop7Application.class, args);
+	}
+
+}
