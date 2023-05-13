6de0238f151999a3b2dbfab7a7176d26ca62e975
madelin <madelintania@gmail.com>
2023-05-13 14:19:39 -0300

    fix proyectos

---
+++ src/main/java/com/mvc/alquileres/controllers/HomeController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mvc.alquileres.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Administrator
+ */
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String index(){
+     return "home";
+    }
+}
