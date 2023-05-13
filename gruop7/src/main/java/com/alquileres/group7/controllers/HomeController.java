956ac22bb5d71a147b370565d328090807aa9473
madelin <madelintania@gmail.com>
2023-05-13 14:30:12 -0300

    fix proyectos

---
+++ gruop7/src/main/java/com/alquileres/group7/controllers/HomeController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.alquileres.gruop7.controller;
+
+/**
+ *
+ * @author Administrator
+ */
+public class AlquilerController {
+    
+}
