7d534255d46c95b40ec5e1b53f88ebaae3e73d7e
madelin <madelintania@gmail.com>
2023-05-29 02:23:11 -0300

    Agrego entidad Cliente y cliente repo, arreglo html

---
+++ src/main/java/com/mvc/alquileres/entities/Cliente.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mvc.alquileres.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+}
